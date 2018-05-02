9a1676723796e3a53073dc32287fbfee3a86ef0c
John Wolfe <wolfejw86@gmail.com>
2018-05-01 23:51:16 -0400

    better routing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
-import express from "express";
+dotenv.config();
+import express, { Request, Response } from "express";
 import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import path from "path";
 import HomeRouter from "./routes/api";
-// Load environment variables from .env file, where API keys and passwords are configured
-dotenv.config();
-
-// Controllers (route handlers)
-import * as apiController from "./controllers/api";
 
 // Create express server
 const app = express();
@@ -19,7 +15,13 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// routing
 app.use("/api", HomeRouter);
+
+const handleUnexpectedError = (err: Error, req: Request, res: Response, next: Function) => {
+  res.status(500).json({ message: "Unexpected Server Error" });
+};
+
+app.use(handleUnexpectedError);
 
 export default app;
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,10 +3,7 @@
 import { Response, Request, NextFunction } from "express";
 
 export class HomeController {
-  public apiHome = (req: Request, res: Response) => {
+  public apiHome = (req: Request, res: Response, next: Function) => {
     res.json({ status: 200 });
   }
-  public handleUnexpectedError = (err: Error, req: Request, res: Response, next: Function) => {
-    res.status(500).json({ message: "Unexpected Server Error" });
-  };
 }
