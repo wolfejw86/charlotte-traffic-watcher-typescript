06d2f8124262664b9bb1bcff70cb5110e100959f
John Wolfe <wolfejw86@gmail.com>
2018-05-02 07:10:45 -0400

    refactored app.ts into a class

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import express, { Request, Response, Application } from "express";
 import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
 import path from "path";
@@ -8,22 +8,34 @@ dotenv.config();
 // routers
 import HomeRouter from "./routes/api";
 
-// Create express server
-const app = express();
 
-// express configuration
-app.set("port", process.env.PORT || 3000);
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+class App {
+  public app: Application;
 
-// routing
-app.use("/api", HomeRouter);
+  constructor() {
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
 
-export const handleUnexpectedError = (err: Error, req: Request, res: Response, next: Function) => {
-  res.status(500).json({ message: "Unexpected Server Error" });
-};
+  private routes() {
+    // routing
+    this.app.use("/api", HomeRouter);
+    const handleUnexpectedError = (err: Error, req: Request, res: Response, next: Function) => {
+      res.status(500).json({ message: "Unexpected Server Error" });
+    };
+    this.app.use(handleUnexpectedError);
+  }
 
-app.use(handleUnexpectedError);
+  private middleware() {
+    // express configuration
+    this.app.set("port", process.env.PORT || 3000);
+    this.app.use(compression());
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
+}
 
-export default app;
+
+
+export default new App().app;
