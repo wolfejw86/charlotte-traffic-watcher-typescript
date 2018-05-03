238cbe79ed65563a3910d60249441b024577dcbe
John Wolfe <wolfejw86@gmail.com>
2018-05-02 21:49:11 -0400

    improved test coverage

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,30 @@
 import request from "supertest";
-import app from "../src/app";
+import app, { App } from "../src/app";
 
-describe("GET /api", () => {
-  it("should return 200 OK", () => {
-    return request(app).get("/api")
-      .expect(200);
+const appTest = new App();
+
+describe("API Tests", () => {
+  describe("GET /api", () => {
+    it("should return 200 OK", () => {
+      return request(app).get("/api")
+        .expect(200);
+    });
+    it("should return JSON data", () => {
+      return request(app).get("/api")
+        .expect(Object);
+    });
+  });
+
+  describe("GET /api/current", () => {
+    it("should return an object", () => {
+      return request(app).get("/api/current")
+        .expect(Object);
+    });
+    it("should return 200 OK", () => {
+      return request(app).get("/api/current")
+        .expect(200);
+    });
   });
 });
+
+
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,22 @@
 import request from "supertest";
-import app from "../src/app";
+import app, { App } from "../src/app";
 
-describe("GET /random-url", () => {
-  it("should return 404", (done) => {
-    request(app).get("/reset")
-      .expect(404, done);
+const appTest = new App();
+
+describe("App tests", () => {
+  it("should be an instance of express.Application", () => {
+    return expect(app).toBeDefined();
+  });
+  it("should correctly load middleware", () => {
+    return expect(App).toBeInstanceOf(Function);
+  });
+  it("should have a property to load middleware", () => {
+    return expect(appTest["middleware"]).toBeInstanceOf(Function);
+  });
+  it("should have a property to load middleware", () => {
+    return expect(appTest["routes"]).toBeInstanceOf(Function);
+  });
+  it("should use a constructor to return an instance", () => {
+    return expect(appTest).toBeInstanceOf(App);
   });
 });
